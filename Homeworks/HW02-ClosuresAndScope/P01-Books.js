ddf7ae34962ff0a3422bdf0a1eb31441a5221e83
Panglot <DobromirBogatev@gmail.com>
2016-09-03 19:35:59 +0300

    HW progress

--- Homeworks/HW02-ClosuresAndScope/P01-Books.js
+++ Homeworks/HW02-ClosuresAndScope/P01-Books.js
@@ -14,7 +14,7 @@
  *	This can be done by author, by category or all
  *	List all categories
  *	Categories are sorted by ID
- *	Each book/catagory has a unique identifier (ID) that is a number greater than or equal to 1
+ *	Each book/category has a unique identifier (ID) that is a number greater than or equal to 1
  *	When adding a book/category, the ID is generated automatically
  *	Add validation everywhere, where possible
  *	Book title and category name must be between 2 and 100 characters, including letters, digits and special characters ('!', ',', '.', etc)
@@ -25,22 +25,35 @@
  */
 function solve() {
 
-    var library = function (bookList) {
-        var books = bookList;
+    var library = (function () {
+        var books = [];
         var categories = [];
+
         function listBooks() {
             return books;
         }
 
-        function addCategory(categoryName) {
-            categories.push(categoryName);
-            return this;
+        function addCategory(categoryToAdd) {
+            categoryToAdd.ID = categories.length + 1;
+            categories.push(categoryToAdd);
+            return categoryToAdd;
         }
 
-        function addBook(book) {
-            book.ID = books.length + 1;
-            books.push(book);
-            return book;
+        function addBook(bookToAdd) {
+            books.forEach(function (currentBook) {
+                if (currentBook.title === bookToAdd.title &&
+                    currentBook.author === bookToAdd.author &&
+                    currentBook.ISBN === bookToAdd.ISBN) {
+                    throw 'Error: This book exists in the database. New books should be unique.';
+                }
+            })
+            if(10 > bookToAdd.ISBN.length || bookToAdd.ISBN.length > 13){
+                throw  'Error: Invalid ISBN. ISBN must be between 10 and 13 symbols.';
+            }
+
+            bookToAdd.ID = books.length + 1;
+            books.push(bookToAdd);
+            return bookToAdd;
         }
 
         function listCategories() {
@@ -57,12 +70,15 @@ function solve() {
                 add: addCategory
             }
         };
-    } ;
+    })();
 
 
-    var newLibrary = new library(['ivan','petkan','dragan']);
+    library.books.add({title: "Harry Potter and the sorcerer's stone", author: "J.K. Rowling", ISBN: "15A23"});
+    library.books.add({title: "Harry Potter and the chamber of secrets", author: "J.K. Rowling", ISBN: "15A23"});
+    library.books.add({title: "Harry Potter and the prisoner of Azkaban", author: "J.K. Rowling", ISBN: "15A23"});
+    library.books.add({title: "Harry Potter and the Goblet of fire", author: "J.K. Rowling", ISBN: "15A23"});
 
-    console.log(newLibrary.books.list());
+    console.log(library.books.list());
 
 
     return library;
